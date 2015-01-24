45a20ef8537641353a55f60d3562b8f26d002ead
Julian K. Arni <jkarni@gmail.com>
2015-01-24 23:18:55 +0800

    Working Auth example.

--- examples/AuthenticationCombinator.hs
+++ examples/AuthenticationCombinator.hs
@@ -1,16 +1,21 @@
 {-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module AuthenticationCombinator where
 
 import Servant
+import Servant.Server.Internal ( succeedWith )
+import Data.ByteString
 import Network.Wai
+import Network.Wai.Handler.Warp ( run )
 import Network.HTTP.Types
 
 -- The combinator
 data WithAuthentication = WithAuthentication
 
-type DBLookup = String -> IO Bool
+type DBLookup = ByteString -> IO Bool
 
 isGoodCookie :: DBLookup
 isGoodCookie = return . (== "myHardcodedCookie")
@@ -27,9 +32,36 @@ instance forall a. HasServer a => HasServer (WithAuthentication :> a) where
     type Server (WithAuthentication :> a) = Server a
 
     -- Here we add the functionality.
-    route Proxy (WithAuthentication :> a) request{..} respond =
-        case lookup "Cookie" requestHeader of
-            Nothing -> responseLBS status401 [] ""
-            Just x  -> if isGoodCookie x
-                   then route (Proxy :: Proxy a) request response
-                   else responseLBS status403 [] ""
+    route Proxy a request respond =
+        case lookup "Cookie" (requestHeaders request) of
+            Nothing -> respond $ succeedWith $ responseLBS status401 [] ""
+            Just x  -> do
+                cookie <- isGoodCookie x
+                if cookie
+                   then route (Proxy :: Proxy a) a request respond
+                   else respond $ succeedWith $ responseLBS status403 [] ""
+
+
+-- Now let's use it!
+
+type MyApi = "home" :> Get Int
+        :<|> "secret" :> SecretApi
+
+type SecretApi = WithAuthentication :> ( "name" :> Get String
+                                    :<|> "age"  :> Get Int )
+
+myApi :: Proxy MyApi
+myApi = Proxy
+
+secretServer :: Server SecretApi
+secretServer = getName :<|> getAge
+    where getName = return "Dread Pirate Roberts"
+          getAge  = return 572
+
+server :: Server MyApi
+server = getHome
+    :<|> secretServer
+  where getHome = return 5
+
+main :: IO ()
+main = run 8090 (serve myApi server)
